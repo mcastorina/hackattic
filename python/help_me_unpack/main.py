65a237b96f0a10015b56a3f23efbf59611ea536e
Miccah Castorina <m.castorina93@gmail.com>
2023-05-13 18:16:21 -0500

    python: refactor help_me_unpack solution

--- python/help_me_unpack/main.py
+++ python/help_me_unpack/main.py
@@ -3,11 +3,22 @@ import requests
 import base64
 import os
 
-challenge = 'https://hackattic.com/challenges/help_me_unpack'
-token = os.environ['TOKEN']
+CHALLENGE = 'help_me_unpack'
+HACK_ATTIC = f'https://hackattic.com/challenges/{CHALLENGE}'
+TOKEN = os.environ['TOKEN']
 
-req = requests.get(f'{challenge}/problem?access_token={token}').json()['bytes']
-raw_bytes = base64.b64decode(req)
+
+def get_challenge(**kwargs):
+    kwargs['access_token'] = TOKEN
+    return requests.get(f'{HACK_ATTIC}/problem', params=kwargs).json()
+
+
+def solve(j, **kwargs):
+    kwargs['access_token'] = TOKEN
+    return requests.post(f'{HACK_ATTIC}/solve', params=kwargs, json=j).json()
+
+
+raw_bytes = base64.b64decode(get_challenge()['bytes'])
 v1 = recast.bytes2int(raw_bytes[0:4])
 v2 = recast.bytes2uint(raw_bytes[4:8])
 v3 = recast.bytes2short(raw_bytes[8:10])
@@ -16,7 +27,4 @@ v5 = recast.bytes2double(raw_bytes[16:24])
 v6 = recast.bytes2double(raw_bytes[24:32], byteorder='big')
 payload = {'int': v1, 'uint': v2, 'short': v3,
            'float': v4, 'double': v5, 'big_endian_double': v6}
-result = requests.post(
-    f'{challenge}/solve?access_token={token}&playground=1', json=payload).json()
-
-print(result)
+print(solve(payload, playground=1))
